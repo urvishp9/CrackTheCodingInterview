f8fd6448c8b70860bf0044228a3bafee4ea3c94f
Patel Urvishkumar <patelurv@accounts.intern>
2019-02-09 14:34:07 -0500

    Rotate Matrix

--- CrackTheCodingInterview/src/main/java/urvish/arraysandstring/example7/Rotate90Degree.java
+++ CrackTheCodingInterview/src/main/java/urvish/arraysandstring/example7/Rotate90Degree.java
@@ -13,12 +13,25 @@ public class Rotate90Degree {
 
     static int N = 4;
 
+    static void rotate90Degrees(int a[][]) {
+        for (int i = 0; i < N / 2; i++) {
+            for (int j = i; j < N - i - 1; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[N - 1 - j][i];
+                a[N - 1 - j][i] = a[N - 1 - i][N - 1 - j];
+                a[N - 1 - i][N - 1 - j] = a[j][N - 1 - i];
+                a[j][N - 1 - i] = temp;
+            }
+        }
+
+    }
+
     static void printMatrix(int a[][]) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(a[i][j] + " ");
             }
-             System.out.println();
+            System.out.println();
         }
     }
 
@@ -27,6 +40,8 @@ public class Rotate90Degree {
         {5, 6, 7, 8},
         {9, 10, 11, 12},
         {13, 14, 15, 16}};
+        printMatrix(a);
+        rotate90Degrees(a);
         printMatrix(a);
     }
 }
